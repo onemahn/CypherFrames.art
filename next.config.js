655b3218e41c38e2e15b721f3ec232546da21f0e
y3j4man <100993216+onemahn@users.noreply.github.com>
2026-07-21 13:13:38 +0000

    Configure static export for Cloudflare Pages

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+
+  images: {
+    unoptimized: true,
+  },
+
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
